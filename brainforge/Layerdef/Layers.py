OH MY GOD there was a major performance bug in the derivative of the sigmoids...

brainforge/Layerdef/Layers.py
@@ -87,7 +87,7 @@ class PoolLayer(_VecLayer):
                     deltas[l, z, start0:end0, start1:end1] += bpfilter * vec[i]
 
         prev = self.brain.layers[self.position-1]
-        return deltas * prev.activation.derivative(prev.excitation)
+        return deltas * prev.activation.derivative(prev.output)
 
     def receive_error(self, error_matrix):
         """
@@ -173,7 +173,7 @@ class ConvLayer(_VecLayer):
                     np.add(deltas[..., start0:end0, start1:end1], diff,
                            out=deltas[..., start0:end0, start1:end1])
         prev = self.brain.layers[self.position-1]
-        return deltas * prev.activation.derivative(prev.excitation)
+        return deltas * prev.activation.derivative(prev.output)
 
     def receive_error(self, error_matrix: np.ndarray):
         """
@@ -266,7 +266,7 @@ class FFLayer(_FCLayer):
         prev = self.brain.layers[self.position-1]
         posh = [self.error.shape[0]] + list(prev.outshape)
         deltas = np.dot(self.error, self.weights.T).reshape(posh)
-        return deltas * prev.activation.derivative(prev.excitation)
+        return deltas * prev.activation.derivative(prev.output)
 
     def weight_update(self):
         """
@@ -328,7 +328,7 @@ class DropOut(FFLayer):
         prev = self.brain.layers[self.position - 1]
         posh = [self.error.shape[0]] + list(prev.outshape)
         deltas = np.dot(self.error, self.weights.T * self.mask.T).reshape(posh)
-        return deltas * prev.activation.derivative(prev.excitation)
+        return deltas * prev.activation.derivative(prev.output)
 
     def weight_update(self):
         # Apply L2 regularization, aka weight decay

brainforge/Layerdef/Recurrence.py
@@ -5,9 +5,10 @@ from csxnet.nputils import ravel_to_matrix as ravtm
 
 
 class RLayer(FFLayer):
-    def __init__(self, brain, inputs, neurons, position, activation):
+    def __init__(self, brain, inputs, neurons, bptt_truncate, position, activation):
         FFLayer.__init__(self, brain, inputs, neurons, position, activation)
         self.rweights = np.random.randn(neurons, neurons)
+        self.bptt_truncate = bptt_truncate
 
     def feedforward(self, questions):
         self.inputs = ravtm(questions)
@@ -22,34 +23,43 @@ class RLayer(FFLayer):
 
     def backpropagation(self):
         """Backpropagation through time (BPTT)"""
-        pass  # OMGGGGGG
+        raise NotImplementedError(":(")
+
+    def receive_error(self, error):
+        self.error = ravtm(error)
+
+    def weight_update(self):
+        dLdU = np.zeros(self.weights.shape)
+        dLdW = np.zeros(self.rweights.shape)
+
+        for bptt in np.arange(max(0, t - self.bptt_truncate), t+1)[::-1]:
+            np.outer(self.error, self.output[bptt - 1])
 
 
-def bptt(self, x, y):
+def bptt_reference(self, x, y):
     T = len(y)
     # Perform forward propagation
+    # Catch network output and hidden output
     o, s = self.forward_propagation(x)
     # We accumulate the gradients in these variables
     dLdU = np.zeros(self.U.shape)
     dLdV = np.zeros(self.V.shape)
     dLdW = np.zeros(self.W.shape)
 
-    # OMG THIS SIMPLY CALCULATES (output - target)...
-    # Which by the way is actually the grad of Xent wrt the outweights
+    # OMG THIS SIMPLY CALCULATES (output - target*)...
+    # Which by the way is the grad of Xent wrt the outweights
     # aka the output error
+    # * provided that target is an array of 1-hot vectors
     delta_o = o
     delta_o[np.arange(len(y)), y] -= 1.
 
-    # For each output backwards...
     for t in np.arange(T)[::-1]:
-        # Calculates the output gradients
-        # TODO: I LEFT IT HERE
+        # This is the sum of the weights' gradients
         dLdV += np.outer(delta_o[t], s[t].T)
-        # Initial delta calculation
+        # backpropagating to the hiddens (W * Er) * tanh'(Z)
         delta_t = self.V.T.dot(delta_o[t]) * (1 - (s[t] ** 2))
         # Backpropagation through time (for at most self.bptt_truncate steps)
         for bptt_step in np.arange(max(0, t - self.bptt_truncate), t + 1)[::-1]:
-            # print "Backpropagation step t=%d bptt step=%d " % (t, bptt_step)
             dLdW += np.outer(delta_t, s[bptt_step - 1])
             dLdU[:, x[bptt_step]] += delta_t
             # Update delta for next step

brainforge/Utility/activations.py
@@ -18,20 +18,8 @@ class Sigmoid(_ActivationFunctionBase):
     def __str__(self): return "sigmoid"
 
     @staticmethod
-    def derivative(Z):
-        return np.multiply(Sigmoid()(Z), np.subtract(1.0, Sigmoid()(Z)))
-
-
-class FastSigmoid(_ActivationFunctionBase):
-
-    def __call__(self, Z):
-        return np.divide(Z, np.add(1, np.abs(Z)))
-
-    def __str__(self): return "fast sigmoid"
-
-    @staticmethod
-    def derivative(Z):
-        return np.divide(1.0, np.add(np.abs(Z), 1.0)**2)
+    def derivative(A):
+        return np.multiply(A, np.subtract(1.0, A))
 
 
 class Tanh(_ActivationFunctionBase):
@@ -42,8 +30,8 @@ class Tanh(_ActivationFunctionBase):
     def __str__(self): return "tanh"
 
     @staticmethod
-    def derivative(Z):
-        return np.subtract(1.0, np.square(Tanh()(Z)))
+    def derivative(A):
+        return np.subtract(1.0, np.square(A))
 
 
 class Linear(_ActivationFunctionBase):
@@ -66,8 +54,8 @@ class ReL(_ActivationFunctionBase):
     def __str__(self): return "relu"
 
     @staticmethod
-    def derivative(Z):
-        return np.greater(0.0, Z).astype("float64")
+    def derivative(A):
+        return np.greater(0.0, A).astype("float32")
 
 
 class SoftMax(_ActivationFunctionBase):
@@ -75,8 +63,8 @@ class SoftMax(_ActivationFunctionBase):
     def __call__(self, Z):
         return np.divide(Z, np.sum(Z, axis=0))
 
-    def __str__(self): return  "softmax"
+    def __str__(self): return "softmax"
 
     @staticmethod
-    def derivative(Z):
+    def derivative(A):
         raise NotImplementedError("Sorry for this...")