1cecb9cdcabad757e482f19c0e8e1fe5a6e5ee48
Csxeba <csxeba@gmail.com>
2016-05-30 16:08:26 +0200

    Still trying to figure out this recurrence thing

--- thNets/thRNN.py
+++ thNets/thRNN.py
@@ -3,48 +3,45 @@ import theano
 import theano.tensor as T
 import theano.tensor.nnet as nnet
 
+
 floatX = theano.config.floatX
+
+
+class ThRNN:
+    def __init_(self, neurons, inputs):
+        fanin = np.prod(inputs)
+
 
 
 class ThLSTM:
     def __init__(self, neurons, inputs):
         fanin = np.prod(inputs)
-        self.forget_input = theano.shared(
-            (np.random.randn(fanin, neurons) / np.sqrt(fanin))
-            .astype(floatX), name="Forget Input Gate")
-        self.input_input = theano.shared(
-            (np.random.randn(fanin, neurons) / np.sqrt(fanin))
-            .astype(floatX), name="Input Input Gate")
-        self.output_input = theano.shared(
-            (np.random.randn(fanin, neurons) / np.sqrt(fanin))
-            .astype(floatX), name="Output Input Gate")
+        self.input_weights = theano.shared(
+            (np.random.randn(4, fanin, neurons) / np.sqrt(fanin))
+            .astype(floatX), name="Input Gates")
 
-        self.forget_state = theano.shared(
-            (np.random.randn(neurons, neurons) / np.sqrt(neurons))
-            .astype(floatX), name="Forget State Gate")
-        self.input_state = theano.shared(
-            (np.random.randn(neurons, neurons) / np.sqrt(neurons))
-            .astype(floatX), name="Input State Gate")
-        self.output_state = theano.shared(
-            (np.random.randn(neurons, neurons) / np.sqrt(neurons))
-            .astype(floatX), name="Output State Gate")
+        self.state_weights = theano.shared(
+            (np.random.randn(4, neurons, neurons) / np.sqrt(neurons))
+            .astype(floatX), name="State Gates")
 
-        self.forget_bias = theano.shared(
-            np.zeros((neurons,), dtype=floatX),
-            name="Forget Bias")
-        self.input_bias = theano.shared(
-            np.zeros((neurons,), dtype=floatX),
-            name="Input Bias")
-        self.output_bias = theano.shared(
-            np.zeros((neurons,), dtype=floatX),
-            name="Output Bias")
+        self.biases = theano.shared(
+            np.zeros((4, neurons), dtype=floatX), name="Biases")
 
         self.cell_state = theano.shared(
-            np.zeros(neurons, neurons).astype(floatX),
-            name="Cell State")
+            np.zeros(neurons, neurons).astype(floatX), name="Cell State")
 
     def output(self, inputs, mint):
-        i = nnet.sigmoid(
-            inputs.dot(self.input_input) +
+        preact = inputs.dot(self.input_weights)
+        preact += self.cell_state.dot(self.state_weights) + self.biases
+        i = nnet.hard_sigmoid(preact[..., :4])
+        f = nnet.hard_sigmoid(preact[..., 4:8])
+        o = nnet.hard_sigmoid(preact[..., 8:12])
+        c_ = T.tanh(preact[..., 12:])
+        c = self.cell_state * f + c_ * i
+        self.cell_state = c
+        return T.tanh(c) * o
 
-        )
+
+class ThGRU:
+    def __init__(self, neurons, inputs):
+        fanin = np.prod(inputs)
