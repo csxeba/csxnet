Fixed the Dense slowing problem by profiling. SoftMax not working :(

ann.py
@@ -162,8 +162,8 @@ class Network(NeuralNetworkBase):
             if verbose:
                 done_percent = int(100 * (((bno + 1) * batch_size) / self.N))
                 print("\r{}%:\tCost: {}\t ".format(done_percent, niceround(np.mean(costs), 5)), end="")
-                if "acc" in monitor:
-                    print_progress()
+        if "acc" in monitor:
+            print_progress()
         print()
         return costs
 

util/_nnutil.py
@@ -110,9 +110,11 @@ class _ReLU(_ActivationFunctionBase):
 class _SoftMax(_ActivationFunctionBase):
 
     def __call__(self, Z):
-        return Z / np.sum(Z, axis=1)[:, None]
+        eZ = np.exp(Z)
+        return eZ / np.sum(eZ, axis=1)[:, None]
 
-    def __str__(self): return "softmax"
+    def __str__(self):
+        return "softmax"
 
     @staticmethod
     def derivative(A):